32ac7e1c828969095e9878061a1c8bb154a576d0
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-13 22:09:18 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ export default {
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -15,15 +15,15 @@ export default {
         },
       },
       {
-        test: /.css$/,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
-        test: /.scss$/,
+        test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
-        test: /.woff2?(\?v=[0-9].[0-9].[0-9])?$/,
+        test: /\.woff2?(\?v=[0-9].[0-9].[0-9])?$/,
         use: 'url-loader?limit=10000',
       },
       {
