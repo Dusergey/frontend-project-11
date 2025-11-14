c1c53635ede21e73168c14155e07dda37dcc5337
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-15 02:06:56 +0300

    Update init.js

--- src/init.js
+++ src/init.js
@@ -49,15 +49,15 @@ export default () => {
   const watchedState = initView(initialState, elements, i18n)
 
   // Обработчик отправки формы
-  elements.form.addEventListener('submit', e => {
+  elements.form.addEventListener('submit', (e) => {
     e.preventDefault()
     watchedState.rssForm.state = 'filling'
     const formData = new FormData(e.target)
     const url = formData.get('url')
-    const urlsList = watchedState.feeds.map(feed => feed.url)
+    const urlsList = watchedState.feeds.map((feed) => feed.url)
 
     validateUrl(url, urlsList, i18n)
-      .then(validUrl => {
+      .then((validUrl) => {
         watchedState.rssForm.error = null
         watchedState.rssForm.state = 'processing'
         return fetchData(validUrl)
@@ -65,12 +65,12 @@ export default () => {
       .then(({ data }) => {
         const [feed, posts] = getFeedAndPosts(data.contents)
         const newFeed = { ...feed, id: _.uniqueId(), url }
-        const newPosts = posts.map(post => ({ ...post, id: _.uniqueId(), feedId: newFeed.id }))
+        const newPosts = posts.map((post) => ({ ...post, id: _.uniqueId(), feedId: newFeed.id }))
         watchedState.feeds = [newFeed, ...watchedState.feeds]
         watchedState.posts = [...newPosts, ...watchedState.posts]
         watchedState.rssForm.state = 'success'
       })
-      .catch(err => {
+      .catch((err) => {
         watchedState.rssForm.valid = err.name !== 'ValidationError'
         if (err.name === 'ValidationError') {
           watchedState.rssForm.error = err.message
