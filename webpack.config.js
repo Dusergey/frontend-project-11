2025e78fdb3faee6fc316f47d4e0a157d7702d26
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-14 23:51:51 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 export default {
   mode: process.env.NODE_ENV || 'development',
@@ -48,4 +48,4 @@ export default {
   output: {
     clean: true,
   },
-};
+}
