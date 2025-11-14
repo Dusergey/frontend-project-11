ba67a309394853ac611f64cb45ee24c26f312550
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-15 00:29:14 +0300

    Update proxy.js

--- src/utils/proxy.js
+++ src/utils/proxy.js
@@ -3,5 +3,5 @@ export default (url, base = 'https://allorigins.hexlet.app/get') => {
   const searchUrl = encodeURI(url);
   newUrl.searchParams.set('disableCache', 'true');
   newUrl.searchParams.set('url', searchUrl);
-  return newUrl;
+  return newUrl
 };
