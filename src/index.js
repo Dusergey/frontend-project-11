719bc676088bf79d1e6922d52078a39da7b3a051
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-15 00:10:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // @ts-check
 
-import runApp from './init.js';
-import './styles.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import runApp from './init.js'
+import './styles.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-runApp();
+runApp()
