c59ca9737aa56b5576afe4936744373716b5db10
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-15 02:08:48 +0300

    Update updater.js

--- src/utils/updater.js
+++ src/utils/updater.js
@@ -5,7 +5,7 @@ import getFeedAndPosts from './parser.js'
 const UPDATE_INTERVAL = 5000 // вынесено в константу
 
 const updatePosts = watchedState => {
-  const { feeds, posts } = watchedState
+  const { feeds, posts } = (watchedState)
   const promises = feeds.map(({ url, id }) => fetchData(url)
     .then(({ data }) => {
       const [, receivedPosts] = getFeedAndPosts(data.contents)
